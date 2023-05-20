95a3ff3ddc564b5011e4d6ed5bf6b4ea775e0142
avihhanaryakumarr <avihhan.official>
2023-05-20 16:00:56 -0400

    first commit

---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './navbar.css';
+
+function Nav() {
+  return (
+    <nav>
+      <a href='https://avihhan.github.io/Personal_Portfolio/'><h3>Portfolio</h3></a>
+      <h1 >Calculator</h1>
+    </nav>
+  );
+}
+
+export default Nav;
